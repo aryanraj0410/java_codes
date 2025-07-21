a9e3cbdd2c2f01eee8252463e9cd4106d0ffb552
aryanraj0410 <ranjanayush287@gmail.com>
2025-07-21 22:18:08 +0530

    Linked List

---
+++ LinkedList/Main.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    LL list = new LL();
+    list.insertFirst(2);
+    list.insertFirst(5);
+    list.insertFirst(8);
+    list.insertFirst(9);
+    list.insertFirst(12);
+    list.insertFirst(15);
+    list.insertFirst(19);
+    list.insertFirst(20);
+    list.insertLast(50);
+    list.insert(55, 6);
+    list.display();
+    System.out.println(list.delete(4));
+    list.display();
+    System.out.println(list.deleteFirst());
+    list.display();
+    System.out.println(list.deleteLast());
+    list.display();
+    }
+}
