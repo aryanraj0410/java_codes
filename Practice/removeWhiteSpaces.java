6bc4833c820d59113f38b3b95182cd7784b859fd
aryanraj0410 <ranjanayush287@gmail.com>
2025-09-09 20:32:44 +0530

    Mostly question done

---
+++ Practice/removeWhiteSpaces.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class removeWhiteSpaces {
+    public static void main(String[] args) {
+        String str = "My Name Is Aryan Raj";
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch != ' '){
+                sb.append(ch);
+            }
+        }
+        String noSpace = sb.toString();
+        System.out.println(noSpace);
+    }
+}
